d9c0b1b80d7c25d84f6f07a628f0caf90ab051ea
louiseoh18 <loh5@jh.edu>
2025-11-20 15:07:47 -0500

    fetcher template

---
+++ data/template_fetcher.R
@@ -0,0 +1,25 @@
+## ------- ADD URL/SOURCE OF DATA HERE -------
+
+
+## ------- WHAT DATA IS THIS -------
+
+
+#################### GRAB DATA ####################
+# load library
+library(blsAPI)
+library(jsonlite)
+library(dplyr)
+library(ggplot2)
+
+# add codes to grab data here
+
+
+
+# add codes to view data
+
+
+
+
+#################### SAVE DATASET INTO RDA ####################
+
+save(DATASET_YOU_WANNA_SAVE, file = here("data/DATASET_NAME.rda"))
