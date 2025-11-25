88300dab7c2384d1acf615156bfe599008b0c481
Mia Silva <miasilva@Mias-MacBook-Pro-8.local>
2025-11-24 20:58:03 -0500

    census income and poverty data

---
+++ data/census_fetcher.R
@@ -0,0 +1,90 @@
+## ------- ADD URL/SOURCE OF DATA HERE -------
+
+# https://walker-data.com/tidycensus/articles/basic-usage.html
+# https://api.census.gov/data/key_signup.html
+
+## ------- WHAT DATA IS THIS -------
+
+# U.S. census data from tidycensus API
+# Select variables of interest
+
+#################### GRAB DATA ####################
+# load library
+
+library(tidyverse)
+library(here)
+library(tidycensus)
+library(purrr)
+
+# add codes to grab data here
+
+# Original data extraction
+readRenviron("~/.Renviron")
+census_api_key("dc64291726fbe5f0b1ba6cb4562565fe33baa574")
+
+# Looking at variables to choose
+View(load_variables(2020, "acs5", cache = TRUE))
+
+# ACS data only goes back to 2005
+years <- c(2005:2019, 2021:2024)
+
+# Pulling median household income
+income_data <- map(years, ~ get_acs(
+  geography = "us",
+  variables = "B19013_001",
+  year = .x,
+  survey = "acs1"
+))
+
+# Adding year
+income_data <- map2(income_data, years, ~ mutate(.x, year = .y))
+
+# Combining years
+combined_income <- income_data %>%
+  bind_rows() %>%
+  mutate(var_name = case_when(
+    variable == "B19013_001" ~ "med_income"
+  )) %>%
+  select(year, var_name, estimate)
+
+# Poverty data
+poverty_data <- map(years, ~get_acs(
+  geography = "us",
+  variables = c("B17001_001", "B17001_002"),
+  year = .x,
+  survey = "acs1"
+))
+
+# Adding year
+poverty_data <- map2(poverty_data, years, ~ mutate(.x, year = .y))
+
+# Combining years and wrangling to get poverty rate
+combined_poverty <- poverty_data %>%
+  bind_rows() %>%
+  select(-moe) %>%
+  group_by(year) %>%
+  pivot_wider(names_from = variable, values_from = estimate) %>%
+  mutate(poverty_rate = B17001_002/B17001_001) %>%
+  select(year, poverty_rate) %>%
+  pivot_longer(cols = poverty_rate, names_to = "var_name", values_to = "estimate") %>%
+  select(year, var_name, estimate)
+
+# Merging income and poverty data
+census_clean <- combined_income %>%
+  bind_rows(combined_poverty) %>%
+  arrange(year)
+
+# add codes to view data
+
+glimpse(census_clean)
+
+head(census_clean)
+
+
+
+#################### SAVE DATASET INTO RDA ####################
+
+save(census_clean, file = here("data/census_clean.rda"))
+
+# naming: datasetname_clean
+# rda file: datasetname_clean.rda
