1ff491b8747f4842894d12739f163f64361dc64e
OliverBrown16 <obrown16@jh.edu>
2025-11-30 12:40:23 -0500

    create join_data file

--- data/BEA_fetcher.R
+++ data/BEA_fetcher.R
@@ -9,79 +9,41 @@ library(here)
 BEA_KEY <- "C77A6C27-03FD-4B8A-996F-CAA388B4ABF1"
 base_url <- "https://apps.bea.gov/api/data/"
 
+# UPDATED CONFIG: Corrected Line Descriptions for Savings and Profits
 bea_config <- list(
-  # Table T10106: GDP & Government Spending
   list(name = "Real_GDP",                   table = "T10106", line_desc = "Gross domestic product"),
   list(name = "Gov_Spending",               table = "T10106", line_desc = "Government consumption expenditures and gross investment"),
-  
-  # Table T20100: Income & Savings
   list(name = "Disposable_Income",          table = "T20100", line_desc = "Disposable personal income"),
-  list(name = "Personal_Saving_Rate",       table = "T20100", line_desc = "Personal saving rate"),
-  
-  # Table T11200: Corporate Profits
-  list(name = "Corporate_Profits",          table = "T11200", line_desc = "Corporate profits with inventory valuation"),
-  
-  # Table T20304: Inflation 
+  # FIX: Updated description to match BEA API text
+  list(name = "Personal_Saving_Rate",       table = "T20100", line_desc = "Personal saving as a percentage"),
+  # FIX: Shortened to ensure match (avoiding potential "IVA" abbreviation mismatches)
+  list(name = "Corporate_Profits",          table = "T11200", line_desc = "Corporate profits"),
   list(name = "Healthcare_Price_Index",     table = "T20304", line_desc = "Health care")
 )
 
 fetch_bea_variable <- function(config_item) {
-  
-  params <- list(
-    UserID = BEA_KEY,
-    Method = "GetData",
-    DataSetName = "NIPA",
-    TableName = config_item$table,
-    Frequency = "Q",
-    Year = "ALL", 
-    ResultFormat = "JSON"
-  )
-  
+  params <- list(UserID = BEA_KEY, Method = "GetData", DataSetName = "NIPA", 
+                 TableName = config_item$table, Frequency = "Q", Year = "ALL", ResultFormat = "JSON")
   response <- GET(base_url, query = params)
+  json_parsed <- fromJSON(content(response, "text", encoding = "UTF-8"))
   
-  if (status_code(response) != 200) {
-    warning(paste("Failed to fetch", config_item$name, "- Status:", status_code(response)))
-    return(NULL)
-  }
-  
-  json_raw <- content(response, "text", encoding = "UTF-8")
-  json_parsed <- fromJSON(json_raw)
-  
-  if (is.null(json_parsed$BEAAPI$Results$Data)) {
-    warning(paste("No data returned for", config_item$name))
-    return(NULL)
-  }
+  if (is.null(json_parsed$BEAAPI$Results$Data)) return(NULL)
   
   raw_df <- json_parsed$BEAAPI$Results$Data
-  
-  df <- raw_df %>%
-    filter(grepl(config_item$line_desc, LineDescription, ignore.case = TRUE))
-  
+  df <- raw_df %>% filter(grepl(config_item$line_desc, LineDescription, ignore.case = TRUE))
   target_line <- head(unique(df$LineNumber), 1)
   
-  df <- df %>% 
-    filter(LineNumber == target_line) %>%
-    mutate(
-      value = as.numeric(gsub(",", "", DataValue)),
-      Year = as.integer(substr(TimePeriod, 1, 4)),
-      Quarter = as.integer(substr(TimePeriod, 6, 6)),
-      Month = (Quarter - 1) * 3 + 1,
-      date = make_date(Year, Month, 1)
-    ) %>%
+  df %>% filter(LineNumber == target_line) %>%
+    mutate(value = as.numeric(gsub(",", "", DataValue)),
+           Year = as.integer(substr(TimePeriod, 1, 4)),
+           Quarter = as.integer(substr(TimePeriod, 6, 6)),
+           Month = (Quarter - 1) * 3 + 1,
+           date = make_date(Year, Month, 1)) %>%
     select(date, value) %>%
     rename_with(~ config_item$name, .cols = "value")
-  
-  return(df)
 }
 
-data_list <- lapply(bea_config, fetch_bea_variable) %>% 
-  compact()
+final_economic_data <- lapply(bea_config, fetch_bea_variable) %>% compact() %>% reduce(full_join, by = "date") %>% arrange(date)
 
-final_economic_data <- data_list %>%
-  reduce(full_join, by = "date") %>%
-  arrange(date)
-
-print(head(final_economic_data))
-print(tail(final_economic_data))
-
+# OVERWRITE the old file with the new variables
 save(final_economic_data, file = here("data/BEA_economic_data.rda"))
---
+++ join_data.R
@@ -0,0 +1,84 @@
+
+library(tidyverse)
+library(zoo)
+library(janitor)
+library(here)
+
+load("./data/anes_clean.rda") #anes_data and anes_codebook
+load("./data/bls_data_clean.rda") #bls_data_clean
+load("./data/census_clean.rda") #census_clean
+load("./data/monthly_approval_clean.rda") #monthly_approval_clean
+load("./data/BEA_economic_data.rda") #final_economic_data
+
+bls_wide <- bls_data_clean %>%
+  filter(date >= "1941-07-01") %>% 
+  select(date, variable, value) %>%
+  pivot_wider(names_from = variable, values_from = value) %>%
+  mutate(consumer_p_index = round(consumer_p_index, 2))
+
+data_wide <- monthly_approval_clean %>%
+  full_join(bls_wide, by = "date") %>%
+  arrange(desc(date)) %>%
+  mutate(
+    President = ifelse(
+      zoo::na.locf(President, na.rm = FALSE) == zoo::na.locf(President, na.rm = FALSE, fromLast = TRUE),
+      zoo::na.locf(President, na.rm = FALSE),
+      NA
+    ),
+    President = case_when(
+      date >= "1941-07-01" & date <= "1945-04-01" ~ "Franklin D. Roosevelt",
+      date >= "1945-05-01" & date <= "1953-01-01" ~ "Harry Truman",
+      date == "1977-01-01" ~ "Jimmy Carter",
+      TRUE ~ President
+    )
+  )
+
+missing_rows <- data_wide %>%
+  filter(date == "1945-04-01" | date == "1953-01-01" | date == "1977-01-01") %>%
+  mutate(President = case_when(
+    President == "Jimmy Carter" ~ "Gerald Ford",
+    President == "Harry Truman" ~ "Dwight D. Eisenhower",
+    President == "Franklin D. Roosevelt" ~ "Harry Truman"
+  ))
+
+data_wide <- data_wide %>%
+  add_row(missing_rows) %>%
+  arrange(desc(date)) %>%
+  janitor::clean_names() 
+
+final_combined_data <- data_wide %>%
+  full_join(final_economic_data, by = "date") %>%
+  arrange(date)
+
+
+final_combined_data <- final_combined_data %>%
+  fill(
+    Real_GDP, 
+    Disposable_Income, 
+    Corporate_Profits, 
+    Healthcare_Price_Index,
+    Gov_Spending,
+    Personal_Saving_Rate,
+    .direction = "down"
+  ) %>%
+  filter(!(is.na(unemployment_rate) & is.na(approval_rating) & is.na(Real_GDP))) %>%
+  
+  group_by(president) %>%
+  mutate(
+    approval_rating = na.approx(approval_rating, na.rm = FALSE),
+    disapproval_rating = na.approx(disapproval_rating, na.rm = FALSE),
+    unsure_rating = na.approx(unsure_rating, na.rm = FALSE)
+  ) %>%
+  ungroup() %>%
+  
+  fill(
+    Real_GDP, Disposable_Income, Corporate_Profits, Healthcare_Price_Index,
+    Gov_Spending, Personal_Saving_Rate,
+    .direction = "up"
+  )
+
+print(colnames(final_combined_data))
+
+visdat::vis_miss(final_combined_data, warn_large_data = FALSE)
+
+save(final_combined_data, file = here("data/complete_data.rda"))
