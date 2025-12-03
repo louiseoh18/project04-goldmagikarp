8c5ef95dbe120e8dc0372752d116f5a86cbf3eb3
Lidio Jaimes Jr <lidiojaimesjr404@gmail.com>
2025-12-02 23:20:17 -0500

    finalized linetypes

--- work/app_historical_comparison.R
+++ work/app_historical_comparison.R
@@ -4,7 +4,8 @@ library(tidyverse)
 library(lubridate)
 load(here("data/complete_data.rda"))
 
-final_combined_data |>
+approval_comparison_data <-
+  final_combined_data |>
   drop_na(1:5) |>
   group_by(president) |>
   mutate(
@@ -53,10 +54,13 @@ ui <- fluidPage(
       selectInput(
         inputId = "president",
         label = "Choose a President:",
-        choices = rev(unique(final_combined_data$president)[
-          !is.na(unique(final_combined_data$president))
-        ]),
-        selected = "Trump (2nd Term)",
+        choices = replace(
+          rev(unique(approval_comparison_data$president)),
+          c(1, 2),
+          rev(unique(approval_comparison_data$president))[c(2, 1)]
+        ),
+        selected = "Donald Trump",
+        #multiple = TRUE,
         selectize = FALSE
       )
     ),
@@ -64,15 +68,6 @@ ui <- fluidPage(
     mainPanel(
       tabsetPanel(
         fluidRow("Approval Ratings", column(12, plotOutput("approval_plot"))),
-        fluidRow(
-          "Economic Indicators",
-          selectInput(
-            inputId = "econ_var",
-            label = "Select Economic Indicator:",
-            choices = econ_labels
-          ),
-          column(10, plotOutput("econ_plot"))
-        )
       )
     )
   )
@@ -82,29 +77,37 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   # Filter data by selected president
   pres_data <- reactive({
-    final_combined_data |> filter(president == input$president)
+    approval_comparison_data |> filter(president %in% input$president)
   })
 
   # Approval rating plot
   output$approval_plot <- renderPlot({
     pres_data() |>
-      ggplot(aes(x = date)) +
+      ggplot(aes(x = months_in_office, linetype = president)) +
       geom_line(aes(y = approval_rating, color = "Approval"), size = 1) +
       geom_line(aes(y = disapproval_rating, color = "Disapproval"), size = 1) +
       geom_line(aes(y = unsure_rating, color = "Unsure"), size = 1) +
       scale_color_manual(values = c("#61D04F", "#DF536B", "#2297E6")) +
-      labs(y = "Rating (%)", x = "Date", color = "Legend") +
-      theme_minimal()
-  })
-
-  # Economic indicator plot
-  output$econ_plot <- renderPlot({
-    var <- input$econ_var
-    friendly_label <- names(econ_labels)[econ_labels == var]
-    pres_data() |>
-      ggplot(aes(x = date, y = .data[[var]])) +
-      geom_line(size = 1) +
-      labs(y = friendly_label, x = "Date") +
+      scale_linetype_manual(
+        values = c(
+          "solid",
+          "dashed",
+          "dotted",
+          "dotdash",
+          "longdash",
+          "twodash",
+          "12",
+          "21",
+          "13",
+          "31",
+          "14",
+          "41",
+          "1122",
+          "2211",
+          "F2"
+        )
+      ) +
+      labs(y = "Rating (%)", x = "Time in Office (months)", color = "Legend") +
       theme_minimal()
   })
 }
